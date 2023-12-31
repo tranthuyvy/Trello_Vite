419a5e9946869d730e4f62656ec563464cc60f78
tranthuyvy <ttv.thuyvy.1544@gmail.com>
2023-12-31 14:28:59 +0700

    dark-light-mode

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,27 @@ import HomeIcon from '@mui/icons-material/Home'
 import { pink } from '@mui/material/colors'
 import Typography from '@mui/material/Typography'
 
+import { useColorScheme } from '@mui/material/styles'
+
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme()
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light')
+      }}
+    >
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  )
+}
+
 function App() {
 
   return (
     <>
+      <ModeToggle />
+      <br />
       <div>hello, ttv</div>
 
       <Typography variant='body2' color='text.secondary'>Test</Typography>
--- src/theme.js
+++ src/theme.js
@@ -1,21 +1,19 @@
-import { createTheme } from '@mui/material/styles'
-import { red } from '@mui/material/colors'
+import { teal, deepOrange, cyan } from '@mui/material/colors'
+import { experimental_extendTheme as extendTheme } from '@mui/material/styles'
 
-// Create a theme instance.
-const theme = createTheme({
-  palette: {
-    mode: 'dark', //default: 'light'
-    primary: {
-      main: '#556cd6'
+const theme = extendTheme({
+  colorSchemes: {
+    light: {
+      palette: {
+        primary: deepOrange,
+        secondary: cyan
+      }
     },
-    secondary: {
-      main: '#19857b'
-    },
-    error: {
-      main: red.A400
-    },
-    text: {
-      secondary: red[500]
+    dark: {
+      palette: {
+        primary: deepOrange,
+        secondary: teal
+      }
     }
   }
 })
