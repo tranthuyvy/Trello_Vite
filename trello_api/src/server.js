5a0d29ec6ddc8faf3ddaf10d8a8614beab857cf4
tranthuyvy <ttv.thuyvy.1544@gmail.com>
2024-01-08 18:54:28 +0700

    MongoDB vs Mongoose

--- trello_api/src/server.js
+++ trello_api/src/server.js
@@ -7,16 +7,6 @@ const hostname = 'localhost'
 const port = 8081
 
 app.get('/', (req, res) => {
-  // eslint-disable-next-line no-console
-  console.log(mapOrder(
-    [{ id: 'id-1', name: 'One' },
-      { id: 'id-2', name: 'Two' },
-      { id: 'id-3', name: 'Three' },
-      { id: 'id-4', name: 'Four' },
-      { id: 'id-5', name: 'Five' }],
-    ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
-    'id'
-  ))
   res.end('<h1>Hello ttv</h1><hr>')
 })
 
