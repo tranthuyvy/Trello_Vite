e19a4bedd3c846fd83b6885ad773a2f4efe4090a
tranthuyvy <ttv.thuyvy.1544@gmail.com>
2024-01-09 22:02:34 +0700

    connect MongoDB Atlast

--- trello_api/src/config/mongodb.js
+++ trello_api/src/config/mongodb.js
@@ -1,1 +1,25 @@
-//
+const MONGODB_URI='mongodb+srv://ttvthuyvy1544:9xRcbF5KLbjxRxv6@cluster-tranthuyvy.e7wwmxh.mongodb.net/?retryWrites=true&w=majority'
+const DATABASE_NAME='trello'
+
+import { MongoClient, ServerApiVersion } from 'mongodb'
+
+let trelloDatabaseInstance = null
+
+const mongoClientInstance = new MongoClient(MONGODB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true
+  }
+})
+
+export const CONNECT_DB = async () => {
+  await mongoClientInstance.connect()
+
+  trelloDatabaseInstance = mongoClientInstance.db(DATABASE_NAME)
+}
+
+export const GET_DB = () => {
+  if (!trelloDatabaseInstance) throw new Error('Must connect Database')
+  return trelloDatabaseInstance
+}
--- trello_api/src/server.js
+++ trello_api/src/server.js
@@ -1,16 +1,28 @@
+/* eslint-disable no-console */
 import express from 'express'
-// import { mapOrder } from '~/utils/sorts.js'
+import { CONNECT_DB, GET_DB } from './config/mongodb'
 
-const app = express()
+const START_SERVER = () => {
+  const app = express()
 
-const hostname = 'localhost'
-const port = 8081
+  const hostname = 'localhost'
+  const port = 8081
 
-app.get('/', (req, res) => {
-  res.end('<h1>Hello ttv</h1><hr>')
-})
+  app.get('/', async (req, res) => {
+    console.log(await GET_DB().listCollections().toArray())
 
-app.listen(port, hostname, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Server is running at http://${ hostname }:${ port }/`)
-})
+    res.end('<h1>Hello ttv</h1><hr>')
+  })
+
+  app.listen(port, hostname, () => {
+    console.log(`Server is running at http://${hostname}:${port}/`)
+  })
+}
+
+CONNECT_DB()
+  .then(() => console.log('Connect MongoDB Atlast'))
+  .then(() => START_SERVER())
+  .catch(error => {
+    console.log(error)
+    process.exit(0)
+  })
