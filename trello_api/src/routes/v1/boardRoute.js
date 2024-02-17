20819d50cb9c7fae4474beec6236a4c921adf516
tranthuyvy <ttv.thuyvy.1544@gmail.com>
2024-02-17 18:23:24 +0700

    .

--- trello_api/src/routes/v1/boardRoute.js
+++ trello_api/src/routes/v1/boardRoute.js
@@ -6,7 +6,7 @@ const Router = express.Router()
 
 Router.route('/')
   .get((req, res) => {
-    res.status(StatusCodes.OK).json({ message: 'GET: API get list boards'})
+    res.status(StatusCodes.OK).json({ message: 'GET: API get list boards' })
   })
 
   .post(boardValidation.createNew)
